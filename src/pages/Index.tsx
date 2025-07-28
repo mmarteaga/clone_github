b7e5c681c489e3765245d8a8faa5b1b6dfe0446e
Mardoqueo Arteaga <43454283+mmarteaga@users.noreply.github.com>
2025-07-28 17:16:37 -0400

    Create Index.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,15 @@
+import Navigation from "@/components/Navigation";
+import Hero from "@/components/Hero";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-gradient-subtle">
+      <Navigation />
+      <main>
+        <Hero />
+      </main>
+    </div>
+  );
+};
+
+export default Index;
